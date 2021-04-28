02562d0a2ad3feb8c2243169043b61a63875b85e
Julio Gutierrez <juliog@knights.ucf.edu>
2021-04-27 22:29:03 -0400

    Remove sudo from rustup

--- setup.sh
+++ setup.sh
@@ -57,7 +57,7 @@ sudo apt install silversearcher-ag
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup.sh
 chmod +x rustup.sh
-sudo ./rustup.sh
+./rustup.sh
 rm ./rustup.sh
 
 echo 'cd ~' >> ~/.bashrc
